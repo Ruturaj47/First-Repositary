6a5404b026f18d88e8269229d8525412a1e3524b
Ruturaj <Ruturaj@LAPTOP-NFMR2JVV.lan>
2025-02-24 11:56:35 +0530

    ok

--- secondproject/src/Abstraction/anotherinterfacewithdefaultmethod.java
+++
@@ -1,8 +0,0 @@
-package Abstraction;
-
-public interface anotherinterfacewithdefaultmethod {
-
-	public default void defaultmethod() {
-		System.out.println("i am from anotherinterfacewithdefaultmethod");
-	}
-}
--- secondproject/src/Abstraction/secondinterface.java
+++
@@ -1,6 +0,0 @@
-package Abstraction;
-
-public interface secondinterface extends firstinterface {
-	public int add();
-
-}
